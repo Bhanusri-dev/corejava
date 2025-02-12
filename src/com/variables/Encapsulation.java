2d223f84f68f07cb642e0980d4b404c24625cef8
Bhanusri-dev <bhanusrichodisetti@gmail.com>
2025-02-12 15:19:21 +0530

    encapsulation

---
+++ src/com/variables/Encapsulation.java
@@ -0,0 +1,21 @@
+package com.variables;
+
+public class Encapsulation {
+	private String name = "sai";
+	private String color = "red";
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+
+}
---
+++ src/com/variables/Encapsulation1.java
@@ -0,0 +1,12 @@
+package com.variables;
+
+public class Encapsulation1 {
+	public static void main(String[] args) {
+		Encapsulation en = new Encapsulation();
+		System.out.println(en.getName());
+		en.setColor("pink");
+		System.out.println(en.getColor());
+		
+	}
+
+}

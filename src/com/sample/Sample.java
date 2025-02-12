c9a3314327e64b968bd3b005d9ebbb521a283295
Bhanusri-dev <bhanusrichodisetti@gmail.com>
2025-02-12 11:01:17 +0530

    deleted

--- src/com/sample/Sample.java
+++
@@ -1,8 +0,0 @@
-package com.sample;
-
-public class Sample {
-	public static void main(String[] args) {
-		System.out.println("Sample");
-	}
-
-}
--- src/com/testing/Testing.java
+++
@@ -1,8 +0,0 @@
-package com.testing;
-
-public class Testing {
-	public static void main(String[] args) {
-		System.out.println();
-	}
-
-}

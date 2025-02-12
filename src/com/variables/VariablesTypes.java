4addd2603e276dd20881656e633ee2faacb6221e
Bhanusri-dev <bhanusrichodisetti@gmail.com>
2025-02-12 10:56:16 +0530

    format

--- src/com/variables/VariablesTypes.java
+++ src/com/variables/VariablesTypes.java
@@ -1,15 +1,15 @@
 package com.variables;
 
 public class VariablesTypes {
-	
+
 	int num = 200;
 	String name = "sai";
-			
+
 	public static void main(String[] args) {
 		VariablesTypes vt = new VariablesTypes();
 		System.out.println(vt.num);
 		System.out.println(vt.name);
-System.out.println("sai");		
+		System.out.println("sai");
 	}
 
 }

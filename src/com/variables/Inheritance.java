ae5ef03c1fd69465a6ccd83a2b90d05665208667
Bhanusri-dev <bhanusrichodisetti@gmail.com>
2025-02-13 18:06:34 +0530

    inheritance

---
+++ src/com/variables/Inheritance.java
@@ -0,0 +1,11 @@
+package com.variables;
+
+public class Inheritance {
+	public int num = 100;
+	public int add(int a, int b) {
+		int c= a+b;
+		return c;
+		
+	}
+
+}
---
+++ src/com/variables/Inheritance1.java
@@ -0,0 +1,18 @@
+package com.variables;
+
+public class Inheritance1 extends Inheritance{
+	public void mul() {
+		int num1 = 50;
+		int num2 = 15;
+		int res = num1*num2;
+		System.out.println(res);
+		}
+	public static void main(String[] args) {
+		Inheritance1 nm = new Inheritance1();
+		System.out.println(nm.num);
+		System.out.println(nm.add(100,200));
+		
+	
+	}
+
+}

f38409c49c024ff063d80543d39e9c7a66419b58
Bhanusri-dev <bhanusrichodisetti@gmail.com>
2025-02-14 17:07:51 +0530

    linkedlist

---
+++ src/com/variables/LinkedList.java
@@ -0,0 +1,18 @@
+package com.variables;
+
+import java.util.List;
+
+public class LinkedList {
+ public static void main(String[] args) {
+	 List<String> l = new LinkedList<>();
+
+    
+     l.add("One");
+     l.add("Two");
+     l.add("Three");
+     l.add("Four");
+     l.add("Five");
+     System.out.println(l);
+
+			}
+}

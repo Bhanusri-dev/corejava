814146b9bfa3c1a9bfb3feef4bf523d43720ee1d
Bhanusri-dev <bhanusrichodisetti@gmail.com>
2025-02-18 14:36:52 +0530

    or operator

--- src/com/variables/Query.sql
+++ src/com/variables/Query.sql
@@ -14,18 +14,23 @@ select sum(esalary) from interview.employee;
 select min(esalary) from interview.employee;
 select max(esalary) from interview.employee;
 
-\\ where
+\\ where-WHERE clause is used to filter records.	
  select * from interview.employee where eid=2;
  select * from interview.employee where esalary<50000;
  select * from interview.employee where esalary between 20000 and 50000;
  select * from interview.employee where ename like s%;
  
- \\order by means sort the order
+ \\order by - sort the result-set in ascending or descending order.
  select * from interview.employee order by esalary desc;
  select * from interview.employee order by esalary asc; 
  
- \\And
+ \\And-AND operator is used to filter records based on more than one condition
  select * from interview.employee where esalary>20000 And ename like 's%';
+ 
+ 
+\\OR-
+select * from interview.employee where depart='IT' or ename like 's%';
+
  
  
  
